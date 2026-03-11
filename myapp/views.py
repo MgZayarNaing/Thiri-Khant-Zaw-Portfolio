6c87164974f7dc1212390cea956cb1aae60a5db1
MgZayarNaing <zayarnaing.st@gmail.com>
2026-03-11 14:13:31 +0630

    model

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .models import TmpBannerModel
 # Create your views here.
 
 def HomePage(request):
-    return render(request, 'index.html' )
+    tmpbanner = TmpBannerModel.objects.all()
+    context = {
+        'tmpbanner' : tmpbanner
+    }
+    return render(request, 'index.html', context )
